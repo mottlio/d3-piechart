cb16925d2c074267af279127d61c97b216e545bd
Michal Mottl <michal.mottl@coleurope.eu>
2018-05-14 16:14:55 +0700

    mapping continents to colors with d3 scheme

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ var minYear = d3.min(birthData, d => d.year);
 var width = 600;
 var height = 600;
 var yearData = birthData.filter(d => d.year === minYear);
+
+var continents = [];
+for(var i = 0; i < birthData.length; i++){
+    var continent = birthData[i].continent;
+    if(continents.indexOf(continent) == -1){
+        continents.push(continent)
+    }
+}
+
+var colorScale = d3.scaleOrdinal()
+                    .domain(continents)
+                    .range(d3.schemeCategory10)
